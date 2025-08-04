fc79e6a9dffdcaae3f2fbf3ae4598c7dc9f37e05
EC2 Default User <ec2-user@ip-172-31-14-187.eu-north-1.compute.internal>
2025-08-04 12:04:22 +0000

    updated cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,4 +2,3 @@
 echo "🧹 Cleaning old files..."
 rm -f /home/ec2-user/swappy-backend/appspec.yml
 rm -f /home/ec2-user/swappy-backend/scripts/post_deploy.sh
-rm -f /home/ec2-user/swappy-backend/scripts/cleanup.sh
